855222ed12ea183ead5ebd5f039e8bc81c5188c5
budisetionugroho123 <budi.setio@mail.sirclo.com>
2021-11-26 08:51:04 +0000

    second commit

---
+++ src/HaloDunia.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Budisetionugroho;
+
+class HaloDUnia
+{
+	public function getMessage()
+	{
+		return "halo dunia dari akademi batch 2";
+	}
+}
---
+++ test/testHaloDunia.php
@@ -0,0 +1,9 @@
+<?php
+
+require __DIR__.'/../vendor/autoload.php';
+
+use Budisetionugroho\HaloDunia;
+
+$haloDunia = new HaloDunia();
+
+echo $haloDunia->getMessage();
